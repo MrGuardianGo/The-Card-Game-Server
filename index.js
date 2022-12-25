579020a0392931616ef4a205b900c90aea7d3bf0
MrGuardianGo <dr.saidongargaonka@gmail.com>
2022-12-25 17:24:04 +0530

    initial commit

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get("/", (req, res) => {
 
 io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://pass-on.vercel.app/",
     credentials: true,
   },
 });
