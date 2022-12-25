b9950644c0bea815eebb93f8b0be632c5e2970d3
MrGuardianGo <dr.saidongargaonka@gmail.com>
2022-12-25 15:54:43 +0530

    new-changes

--- index.js
+++ index.js
@@ -5,7 +5,30 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors({origin: ['http://localhost:3000', 'http://127.0.0.1:3000']}));
+app.use(cors());
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8888");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 let PORT = process.env.PORT || 8000;
 
@@ -13,9 +36,9 @@ const server = app.listen(PORT, () => {
   console.log("Changes made on", PORT);
 });
 
-app.get('/', (req, res) => {
-  res.send('Server is up and running!')
-})
+app.get("/", (req, res) => {
+  res.send("Server is up and running!");
+});
 
 io = socket(server, {
   cors: {
