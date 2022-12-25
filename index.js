9d1173c35226bd2248abead972e23a48c894950c
MrGuardianGo <dr.saidongargaonka@gmail.com>
2022-12-25 16:05:38 +0530

    initial commit

--- index.js
+++ index.js
@@ -7,26 +7,17 @@ const app = express();
 
 app.use(cors());
 app.use(function (req, res, next) {
-  // Website you wish to allow to connect
   res.setHeader("Access-Control-Allow-Origin", "*");
-
-  // Request methods you wish to allow
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
 
-  // Request headers you wish to allow
   res.setHeader(
     "Access-Control-Allow-Headers",
     "X-Requested-With,content-type"
   );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
   res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
   next();
 });
 
@@ -222,6 +213,7 @@ io.on("connection", (socket) => {
     socket.broadcast
       .to(data.roomID)
       .emit("winners", { winners: Array.from(winners) });
+    roomsDetails.delete(data.roomID);
   });
   socket.on("disconnect", () => {
     if (playerRooms.get(socket.id)) {
